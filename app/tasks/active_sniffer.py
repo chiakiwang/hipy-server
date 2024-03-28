70506c535c447757499f8ca3cc58ed1089fc00cb
晚风拂柳颜 <434857005@qq.com>
2024-03-28 19:19:11 +0800

    激活脚本优化

--- app/tasks/active_sniffer.py
+++ app/tasks/active_sniffer.py
@@ -27,7 +27,7 @@ def active_sniffer_after_run(*args, **kwargs):
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     logger.info(f"active_sniffer.py执行一次定时任务 {now} ......")
     try:
-        r = requests.get(f'http://localhost:{settings.PORT}/sniffer?active=1', timeout=2)
+        r = requests.get(f'http://127.0.0.1:{settings.PORT}/sniffer?active=1', timeout=2)
         msg += r.text
     except Exception as e:
         msg += f'激活嗅探器发生错误:{e}'
