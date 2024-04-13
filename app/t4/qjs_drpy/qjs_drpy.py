bea5df6bf422d0a1ba4de6dd658f3f69c1eef84e
晚风拂柳颜 <434857005@qq.com>
2024-04-13 10:16:12 +0800

    增加农民新模板

--- app/t4/qjs_drpy/qjs_drpy.py
+++ app/t4/qjs_drpy/qjs_drpy.py
@@ -155,30 +155,19 @@ class Drpy:
 
 if __name__ == '__main__':
     drpy = Drpy(debug=1)
-    # drpy.init('https://ghproxy.liuzhicong.com/https://github.com/hjdhnx/dr_py/raw/main/js/荐片.js')
-    # drpy.init('https://ghproxy.liuzhicong.com/https://github.com/hjdhnx/dr_py/raw/main/js/555影视[飞].js')
-    # drpy.init('https://ghproxy.liuzhicong.com/https://github.com/hjdhnx/dr_py/raw/main/js/农民影视.js')
-    # drpy.init('https://ghproxy.liuzhicong.com/https://github.com/hjdhnx/dr_py/raw/main/js/996影视.js')
-    # drpy.init('https://ghproxy.liuzhicong.com/https://github.com/hjdhnx/dr_py/raw/main/js/奇珍异兽.js')
-    # with open('../files/drpy_js/freeok.js', encoding='utf-8') as f:
-    # with open('../files/drpy_js/农民影视.js', encoding='utf-8') as f:
-    with open('../files/drpy_js/耐看.js', encoding='utf-8') as f:
+    with open('../files/drpy_js/农民影视新.js', encoding='utf-8') as f:
         code = f.read()
     drpy.init(code)
     drpy.setDebug(1)
     print(drpy.homeContent())
-    # print(drpy.homeVideoContent())
+    print(drpy.homeVideoContent())
+    print(drpy.categoryContent('2', 1, False, {}))
+    print(drpy.searchContent("斗罗大陆", False, 1))
+    print(drpy.detailContent('https://m.emsdn.cn/vod-detail-id-38818.html'))
     # f = quickjs.Function(
     #     "adder", """
     #             function adder(x, y) {
     #                 return x + y;
     #             }
     #             """)
-
-    # print(drpy.categoryContent('3', 1, False, {}))
-    # print(drpy.categoryContent('2', 1, False, {}))
-    # print(drpy.detailContent("3$/detail/790.html"))
-    # print(drpy.detailContent("https://nkvod.com/detail/185851.html"))
-    # print(drpy.playerContent("索尼", "https://www.cs1369.com/play/790-1-1.html", []))
-    print(drpy.playerContent("量子资源", "https://nkvod.com/play/185851-2-1.html", []))
-    # print(drpy.searchContent("斗罗大陆", False, 1))
+    print(drpy.playerContent("线路①", "https://m.emsdn.cn/vod-play-id-38818-src-1-num-1.html", []))
