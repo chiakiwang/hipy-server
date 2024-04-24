6e4f7e85893d244e09a26f0dacd5843181f767cd
晚风拂柳颜 <434857005@qq.com>
2024-04-25 00:29:10 +0800

    完善二级内容

--- app/t4/files/drpy_js/白嫖影视.js
+++ app/t4/files/drpy_js/白嫖影视.js
@@ -1,7 +1,8 @@
-muban.首图2.二级.title = '.stui-content__detail .title--span&&Text;.stui-content__detail p:eq(-2)&&Text';
+muban.首图2.二级.title = '.stui-content__detail .title--span&&Text;.stui-content__detail p&&a&&Text';
 muban.首图2.二级.content = 'p.col-pd&&Text';
 muban.首图2.二级.tabs = '.stui-pannel_hd h3';
 muban.首图2.二级.lists = '.stui-content__playlist:eq(#id) li';
+muban.首图2.二级.desc = 'p.data:eq(3)&&Text;p.data&&a:eq(-1)&&Text;p.data&&a:eq(-2)&&Text;p.data:eq(1)&&Text;p.data:eq(2)&&Text';
 var rule = {
 	title: '白嫖影视',
 	模板: '首图2',
@@ -10,6 +11,5 @@ var rule = {
     searchUrl:'/search/page/fypage/wd/**.html',
 	filterable: 0,//是否启用分类筛选,
 	tab_exclude:'影片|评论|榜单',
-    搜索: muban.首图2.搜索2,
 	lazy: ``,
 }
