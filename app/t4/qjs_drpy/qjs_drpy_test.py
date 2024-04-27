6ed31e5e3589f0e9cf651e13c19136d23ad72656
晚风拂柳颜 <434857005@qq.com>
2024-04-27 22:44:24 +0800

    改json文件

--- app/t4/qjs_drpy/qjs_drpy_test.py
+++ app/t4/qjs_drpy/qjs_drpy_test.py
@@ -19,7 +19,9 @@ if __name__ == '__main__':
     # with open('../files/drpy_js/农民影视.js', encoding='utf-8') as f:
     # with open('../files/drpy_js/农民影视新.js', encoding='utf-8') as f:
     # with open('../files/drpy_js/小蚂蚁资源2.js', encoding='utf-8') as f:
-    with open('../files/drpy_js/爱看机器人.js', encoding='utf-8') as f:
+    # with open('../files/drpy_js/爱看机器人.js', encoding='utf-8') as f:
+    with open('../files/drpy_js/荐片.js', encoding='utf-8') as f:
+    # with open('../files/drpy_js/我的哔哩.js', encoding='utf-8') as f:
     # with open('../files/drpy_js/腾云驾雾.js', encoding='utf-8') as f:
     # with open('../files/drpy_js/非凡资源.js', encoding='utf-8') as f:
     # with open('../files/drpy_js/白嫖影视.js', encoding='utf-8') as f:
@@ -29,7 +31,7 @@ if __name__ == '__main__':
     code = code.replace('$nmjx_url', 'http://127.0.0.1:5708/nm?all=&url=')
     drpy.init(code)
     drpy.setDebug(1)
-    # print(drpy.homeContent())
+    print(drpy.homeContent())
     # print(drpy.homeVideoContent())
     # print(drpy.categoryContent('2', 1, False, {}))
     # print(drpy.detailContent('https://v.ikanbot.com/play/846474'))
@@ -54,5 +56,5 @@ if __name__ == '__main__':
     # print(drpy.detailContent("https://nkvod.com/detail/185851.html"))
     # print(drpy.playerContent("索尼", "https://www.cs1369.com/play/790-1-1.html", []))
     # print(drpy.playerContent("量子资源", "https://nkvod.com/play/185851-2-1.html", []))
-    print(drpy.playerContent("优质", "https://yzzy.play-cdn21.com/20240329/13829_b63e7c01/index.m3u8", []))
+    # print(drpy.playerContent("优质", "https://yzzy.play-cdn21.com/20240329/13829_b63e7c01/index.m3u8", []))
     # print(drpy.searchContent("斗罗大陆", False, 1))
